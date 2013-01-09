a92a9aa5f43b9d9904b6d21144029306b561bc76
Odd Eivind Ebbesen <odd@oddware.net>
2013-01-10 00:21:48 +0100

    Fixed a bug in the caching logic

--- LHLunchCache.pm
+++ LHLunchCache.pm
@@ -44,13 +44,18 @@ sub cache_file {
    return $self->{cfile};
 }
 
+sub is_fresh {
+	my ($dt_cache, $dt_cmp) = @_;	# assumed to be DateTime instances
+	return ($dt_cache->ymd('') == $dt_cmp->ymd(''));	# within same day
+}
+
 sub cache {
    my $self      = shift;
    my $dt_parsed = DateTime->from_epoch(epoch => $self->{lhl}->stamp);
    my $dt_now    = DateTime->now;
 
    if ($self->{lhl}->ready) {
-      if ($dt_parsed->ymd('') < $dt_now->ymd('')) {    # midnight has passed
+      if (!is_fresh($dt_parsed, $dt_now)) {    # midnight has passed
          $self->{lhl}->clear->scrape;
       }
    }
@@ -62,7 +67,14 @@ sub cache {
             $self->{lhl}->scrape;
          }
          else {
-            $self->{lhl}->reload($data);
+         		# need to check age here before just reloading...
+         		my $dt_cache = DateTime->from_epoch(epoch => $data->[0]{date});
+         		if (!is_fresh($dt_cache, $dt_now)) {
+         			$self->{lhl}->scrape;
+         		}
+         		else {
+            		$self->{lhl}->reload($data);
+            	}
          }
       }
       else {

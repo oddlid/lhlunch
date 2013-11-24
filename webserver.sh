c55973d6266b239c56a65a053bc4430fc1c3fe37
Odd Eivind Ebbesen <odd@oddware.net>
2013-11-24 14:02:27 +0100

    Updated webserver.sh to include full path to hypnotoad

--- webserver.sh
+++ webserver.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+cd $(dirname $0)
+
 script="LHLunchWebService.pl"
 
-if [ $1 == "start" ] ; then
-   MOJO_REACTOR=Mojo::Reactor::Poll hypnotoad $script
-elif [ $1 == "stop" ] ; then
+if [[ $1 == "start" ]] ; then
+   MOJO_REACTOR=Mojo::Reactor::Poll /usr/bin/vendor_perl/hypnotoad $script
+elif [[ $1 == "stop" ]] ; then
    hypnotoad --stop $script
-elif [ $1 == "status" ] ; then
+elif [[ $1 == "status" ]] ; then
    ps aux | grep $script | grep -v grep
 else
-   echo "Usage: $0 < start | stop >"
+   echo "Usage: $0 < start | status | stop >"
 fi
 

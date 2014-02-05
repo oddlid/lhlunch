b0294a2d3a985dfab3ad9bb1f4ab715fca4616d6
Odd E. Ebbesen <odd.ebbesen@wirelesscar.com>
2014-02-05 11:25:58 +0100

    Changed URL format to dynamic query

--- LHLunchConfig.pm
+++ LHLunchConfig.pm
@@ -28,10 +28,31 @@ use Mojo::UserAgent;
 #   return ($r->{name}, $r->{url}, [ map { { dish => $_->[0], desc => $_->[1], price => $_->[2] } } @d ]);
 #};
 
+# New URL format:
+# http://www.lindholmen.se/pa-omradet/dagens-lunch?type=All&date%5Bvalue%5D%5Byear%5D=2014&date%5Bvalue%5D%5Bmonth%5D=2&date%5Bvalue%5D%5Bday%5D=5&restaurant=143
+# 
+
+sub geturl {
+   my $restaurant_id = shift;
+   my $dt            = DateTime->now;
+   my %u = (
+      '[' => '%5B',
+      ']' => '%5D',
+   );
+   my $url = 'http://www.lindholmen.se/pa-omradet/dagens-lunch?';
+   my $q   = sprintf(
+      "type=All&date%svalue%s%syear%s=%d&date%svalue%s%smonth%s=%d&date%svalue%s%sday%s=%d&restaurant=%d",
+      $u{'['}, $u{']'},    $u{'['}, $u{']'}, $dt->year, $u{'['}, $u{']'},  $u{'['},
+      $u{']'}, $dt->month, $u{'['}, $u{']'}, $u{'['},   $u{']'}, $dt->day, $restaurant_id
+   );
+   return $url . $q;
+}
+
 my $parser = sub {
-   my $r  = shift;
-   my $ua = Mojo::UserAgent->new; # should make the sub get this from the outside, for reuse
-   my $tx = $ua->get($r->{url});
+   my $r   = shift;
+   my $ua  = Mojo::UserAgent->new;    # should make the sub get this from the outside, for reuse
+   my $url = geturl($r->{rid});
+   my $tx  = $ua->get($url);
    my @d;
 
    $tx->res->dom('span[class="dish-name"]')->each(
@@ -46,59 +67,72 @@ my $parser = sub {
       $price =~ s/[^0-9]//g;
       push(@{ $d[$i] }, $price);
    }
-   return ($r->{name}, $r->{url}, [ map { { dish => $_->[0], desc => $_->[1], price => $_->[2] } } @d ]);
+   return ($r->{name}, $url, [ map { { dish => $_->[0], desc => $_->[1], price => $_->[2] } } @d ]);
 };
 
 our @sources = (
    {  name   => q/Bistrot/,
-      url    => 'http://www.lindholmen.se/restauranger/bistrot',
+      #url    => 'http://www.lindholmen.se/restauranger/bistrot',
+      rid    => 129,
       parser => $parser,
    },
    {  name   => q/Encounter Asian Cuisine/,
-      url    => 'http://www.lindholmen.se/restauranger/encounter-asian-cuisine',
+      #url    => 'http://www.lindholmen.se/restauranger/encounter-asian-cuisine',
+      rid    => 141,
       parser => $parser,
    },
    {  name   => q/Semcon/,
-      url    => 'http://www.lindholmen.se/restauranger/lindholmens-matsal-semcon',
+      #url    => 'http://www.lindholmen.se/restauranger/lindholmens-matsal-semcon',
+      rid    => 137,
       parser => $parser,
    },
    {  name   => q/Kooperativet/,
-      url    => 'http://www.lindholmen.se/restauranger/kooperativet',
+      #url    => 'http://www.lindholmen.se/restauranger/kooperativet',
+      rid    => 142,
       parser => $parser,
    },
    {  name   => q/L's Kitchen/,
-      url    => 'http://www.lindholmen.se/restauranger/ls-kitchen',
+      #url    => 'http://www.lindholmen.se/restauranger/ls-kitchen',
+      rid    => 147,
       parser => $parser,
    },
    {  name   => q/Mimolett/,
-      url    => 'http://www.lindholmen.se/restauranger/mimolett',
+      #url    => 'http://www.lindholmen.se/restauranger/mimolett',
+      rid    => 143,
       parser => $parser,
    },
    {  name   => q/Gothia/,
-      url    => 'http://www.lindholmen.se/restauranger/restaurang-gothia',
+      #url    => 'http://www.lindholmen.se/restauranger/restaurang-gothia',
+      rid    => 128,
       parser => $parser,
    },
    {  name   => q/Pir 11 (Ericsson-huset)/,
-      url    => 'http://www.lindholmen.se/restauranger/pir-11-ericsson-huset',
+      #url    => 'http://www.lindholmen.se/restauranger/pir-11-ericsson-huset',
+      rid    => 151,
       parser => $parser,
    },
    {  name   => q/Cuckoo's Nest/,
-      url    => 'http://www.lindholmen.se/restauranger/cuckoos-nest',
+      #url    => 'http://www.lindholmen.se/restauranger/cuckoos-nest',
+      rid    => 157,
       parser => $parser,
    },
    {  name   => q/Tableau/,
-      url    => 'http://www.lindholmen.se/restauranger/restaurang-tableau-tv-och-radiohuset',
+      #url    => 'http://www.lindholmen.se/restauranger/restaurang-tableau-tv-och-radiohuset',
+      rid    => 125,
       parser => $parser,
    },
    {  name   => q/Äran/,
-      url    => 'http://www.lindholmen.se/restauranger/restaurang-aran',
+      #url    => 'http://www.lindholmen.se/restauranger/restaurang-aran',
+      rid    => 139,
       parser => $parser,
    },
    {  name   => q/Sweet and sour/,
-      url    => 'http://www.lindholmen.se/restauranger/sweet-and-sour',
+      #url    => 'http://www.lindholmen.se/restauranger/sweet-and-sour',
+      rid    => 152,
       parser => $parser,
    },
 );
+
 
 1;
 __END__
